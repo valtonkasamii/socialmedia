98eb5991e1966df56e2ccb6f7235357d41f25f8e
Valton Kasami <valtonkasamii@outlook.com>
2024-10-04 18:26:52 +0200

    login

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -58,8 +58,8 @@ const LoginPage = () => {
         <form onSubmit={handleSubmit} className='max-xl:pr-10 max-lg:pr-0 flex flex-col space-y-3'>
             <input required name='username' value={formData.username} onChange={handleInputChange} type='text' className='bg-black border-2 border-gray-500 rounded-[5px] px-[5px] h-[50px] w-[300px] text-[20px]' placeholder='Username'/>
             <input required name='password' value={formData.password} onChange={handleInputChange} type='password' className='bg-black border-2 border-gray-500 rounded-[5px] px-[5px] h-[50px] w-[300px] text-[20px]' placeholder='Password'/>
-            <button className='bg-blue-500  rounded-[5px] py-2 text-[20px] font-[500]'>Login</button>
-            <button className='bg-gray-500 cursor-not-allowed rounded-[5px] py-2 text-[20px] font-[500]'>Loading</button>
+            {login && <button className='bg-blue-500  rounded-[5px] py-2 text-[20px] font-[500]'>Login</button>}
+            {loading && <div className='bg-gray-500 cursor-not-allowed rounded-[5px] py-2 text-[20px] font-[500]'>Loading</div>}
         </form>
         </div>
         <p className='text-xl pt-2 text-red-500'>{err}</p>
