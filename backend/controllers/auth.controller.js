560aec3aedbce1d6ba08244271bded11cdde7a41
Valton Kasami <valtonkasamii@outlook.com>
2024-10-04 18:07:45 +0200

    cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,7 +88,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-    res.cookie("jwt", "", {maxAge: 0})
+    res.cookie("jwt", "", {
+        maxAge: 0, 
+        sameSite: "None",
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+    })
     res.status(200).json({ message: "Logged out successfully" })
   } catch (error) {
     console.log("Error in logout controller", error.message)
